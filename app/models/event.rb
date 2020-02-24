aa77c1b8a2b94d48b7080523e0ca693986a627d0
Milan Vilov <milanvilov@gmail.com>
2020-02-24 22:24:05 +0100

    Refactored and added tests.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,24 @@
 class Event < ApplicationRecord
   def self.availabilities(start_date)
+    raise ArgumentError unless valid_argument(start_date)
+
     availabilities = Hash.new
     end_date = start_date + 6.day
-    openings(start_date, end_date).or(appointments(start_date, end_date)).group_by { |event| event.starts_at.to_date.wday }.map { |day, events|
-      key = get_date(start_date, end_date, day).strftime("%Y/%m/%d")
-      availabilities[key] = create_availability(events)
-    }
 
-    start_date.upto(end_date.to_date).map { |day|
-      {
-        :date => day.strftime("%Y/%m/%d"),
-        :slots => availabilities[day.strftime("%Y/%m/%d")] == nil ? [] : availabilities[day.strftime("%Y/%m/%d")],
-      }
+    #doing and chaining multiple scopes so that it would be one query to db
+    # to load data and process in memory and avoid n+1 problem
+    openings_and_appointments(start_date, end_date)
+      .group_by { |event| event.starts_at.to_date.wday } #group by wday instead of date to overlap with recurring openings
+      .map { |day, events|
+      date_for_wday = get_date(start_date, end_date, day)
+      availabilities[date_for_wday] = free_slots(events)
     }
+    #this grouping by wday works for 7 days range , in case of more then
+    #that we would probably need to check for the comparison
+    #between current date and reccuring openings for that day using
+    # sql query like (strftime('%w', '2018-08-04') as integer) and compare with
+    # date.wday ( query mentioned is compatible with SQLite)
+    map_to_response_model(start_date, end_date, availabilities)
   end
 
   scope :same_day_of_week_but_earlier, ->(start_date) {
@@ -31,6 +37,10 @@ class Event < ApplicationRecord
       where(:kind => "appointment").where(starts_at: start_date.beginning_of_day..end_date.end_of_day)
     }
 
+  scope :openings_and_appointments, ->(start_date, end_date) {
+          openings(start_date, end_date).or(appointments(start_date, end_date))
+        }
+
   def self.get_date(start_date, end_date, wday)
     date = nil
     start_date.upto(end_date).each { |day|
@@ -39,23 +49,46 @@ class Event < ApplicationRecord
         break
       end
     }
-    date
+    date.strftime("%Y/%m/%d")
   end
 
-  def self.create_availability(events)
+  def self.free_slots(events)
     #using Hash as searching trough key is fast for these type of usecases
     slots = Hash.new
     events.each { |event|
       if event.kind == "opening"
-        (event.starts_at.to_datetime.to_i..(event.ends_at.to_datetime - 30.minutes).to_i).step(30.minutes) { |slot|
-          slots[Time.at(slot).utc.strftime("%-H:%M")] = true
-        }
+        add_slots(event, slots)
       elsif event.kind == "appointment"
-        (event.starts_at.to_datetime.to_i..(event.ends_at.to_datetime - 30.minutes).to_i).step(30.minutes) { |slot|
-          slots[Time.at(slot).utc.strftime("%-H:%M")] = false
-        }
+        remove_slots(event, slots)
       end
     }
     slots = slots.filter { |key, value| value }.keys
+  end
+
+  def self.add_slots(event, slots)
+    (event.starts_at.to_datetime.to_i..(event.ends_at.to_datetime - 30.minutes).to_i)
+      .step(30.minutes) { |slot|
+      slots[Time.at(slot).utc.strftime("%-H:%M")] = true
+    }
+  end
+
+  def self.remove_slots(event, slots)
+    (event.starts_at.to_datetime.to_i..(event.ends_at.to_datetime - 30.minutes).to_i)
+      .step(30.minutes) { |slot|
+      slots[Time.at(slot).utc.strftime("%-H:%M")] = false
+    }
+  end
+
+  def self.valid_argument(date)
+    date.is_a?(Date)
+  end
+
+  def self.map_to_response_model(start_date, end_date, availabilities)
+    start_date.upto(end_date.to_date).map { |day|
+      {
+        :date => day.strftime("%Y/%m/%d"),
+        :slots => availabilities[day.strftime("%Y/%m/%d")] == nil ? [] : availabilities[day.strftime("%Y/%m/%d")],
+      }
+    }
   end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -6,7 +6,6 @@ class EventTest < ActiveSupport::TestCase
     Event.create kind: "appointment", starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
 
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
-    p availabilities.to_json
     assert_equal "2014/08/10", availabilities[0][:date]
     assert_equal [], availabilities[0][:slots]
     assert_equal "2014/08/11", availabilities[1][:date]
@@ -16,5 +15,48 @@ class EventTest < ActiveSupport::TestCase
     assert_equal 7, availabilities.length
   end
 
-  #two openings same day
+  test "that .availabities raises exception if argument not date" do
+    assert_raises ArgumentError do
+      availabilities = Event.availabilities SecureRandom.random_number
+    end
+    assert_raises ArgumentError do
+      availabilities = Event.availabilities SecureRandom.hex
+    end
+  end
+
+  test "that all slots are taken if opening and appointment time and date overlap" do
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+    Event.create kind: "appointment", starts_at: DateTime.parse("2014-08-11 09:30"), ends_at: DateTime.parse("2014-08-11 12:30")
+
+    availabilities = Event.availabilities DateTime.parse("2014-08-11")
+    assert_equal "2014/08/11", availabilities[0][:date]
+    assert_equal [], availabilities[0][:slots]
+  end
+
+  test "that two recurring openings same day different time are both added to open slots" do
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 10:30"), weekly_recurring: true
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-04 11:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+
+    availabilities = Event.availabilities DateTime.parse("2014-08-11")
+    assert_equal "2014/08/11", availabilities[0][:date]
+    assert_equal 4, availabilities[0][:slots].count
+  end
+
+  test "that one recurring one fixed opening same day different time are both added to open slots" do
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 10:30"), weekly_recurring: true
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-11 11:30"), ends_at: DateTime.parse("2014-08-11 12:30"), weekly_recurring: false
+
+    availabilities = Event.availabilities DateTime.parse("2014-08-11")
+    assert_equal "2014/08/11", availabilities[0][:date]
+    assert_equal 4, availabilities[0][:slots].count
+  end
+
+  test "that two openings same start different end time do not add duplicate slots" do
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 10:30"), weekly_recurring: true
+    Event.create kind: "opening", starts_at: DateTime.parse("2014-08-11 09:30"), ends_at: DateTime.parse("2014-08-11 12:30"), weekly_recurring: false
+
+    availabilities = Event.availabilities DateTime.parse("2014-08-11")
+    assert_equal "2014/08/11", availabilities[0][:date]
+    assert_equal 6, availabilities[0][:slots].count
+  end
 end
